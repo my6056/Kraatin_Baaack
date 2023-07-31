02b689db32b3b867a36d5f74ebc3007c2d85c59a
my6056 <129304542+my6056@users.noreply.github.com>
2023-07-31 10:39:28 +0000

    Update MedicinsRoutes.js

--- routers/MedicinsRoutes.js
+++ routers/MedicinsRoutes.js
@@ -4,12 +4,15 @@ const router = express.Router();
 const {
   AddMedicins,
   GetMedicins,
+  DeleteMedicin,
 } = require("../controllers/MedicinController");
 
 // aadding medicins routes
 router.post("/add-new", AddMedicins);
 // Get all Medicins
 router.get("/get-all", GetMedicins);
+// delete medici
+router.delete("/delete/:id", DeleteMedicin);
 
 // Export for global use
 module.exports = router;
